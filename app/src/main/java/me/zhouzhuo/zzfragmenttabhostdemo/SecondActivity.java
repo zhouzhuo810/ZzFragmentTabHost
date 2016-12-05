cbb343091eda1eacd952a0db5670af360ea8fc63
zhouzhuo810 <admin@zhouzhuo.me>
2016-12-05 15:53:55 +0800

    添加注释

--- app/src/main/java/me/zhouzhuo/zzfragmenttabhostdemo/SecondActivity.java
+++ app/src/main/java/me/zhouzhuo/zzfragmenttabhostdemo/SecondActivity.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.ViewGroup;
 
-import me.zhouzhuo.zzandroidutils.display.DisplayUtil;
 import me.zhouzhuo.zzfragmenttabhost.ZzFragmentTabHost;
 import me.zhouzhuo.zzfragmenttabhost.ZzTab;
+import me.zhouzhuo.zzfragmenttabhostdemo.utils.DisplayUtil;
 
 /**
  * Created by zz on 2016/10/27.
@@ -21,42 +21,43 @@ public class SecondActivity extends FragmentActivity {
         DisplayUtil.scaleContentView((ViewGroup) findViewById(R.id.act_second));
 
         ZzFragmentTabHost zft = (ZzFragmentTabHost) findViewById(R.id.zft);
-        zft.setUp(this, getSupportFragmentManager(), R.id.fgm_container)
-                .initTabsWithCount(4)
-                .setFragmentClazz(
+
+        zft.setUp(this, getSupportFragmentManager(), R.id.fgm_container) //设置fragment的容器FrameLayout的id
+                .initTabsWithCount(4) //设置Fragment的数量
+                .setFragmentClazz(      //设置Fragment的class
                         FragmentOne.class,
                         FragmentTwo.class,
                         FragmentThree.class,
                         FragmentFour.class)
-                .setTabNameWithResId(R.array.tab_names)
-                .setImagePressIds(
+                .setTabNameWithResId(R.array.tab_names)   //设置Tab文字resid
+                .setImagePressIds(                      //设置tab按下时图片id
                         R.drawable.mainicon_checked,
                         R.drawable.mainicon_checked,
                         R.drawable.mainicon_checked,
                         R.drawable.mainicon_checked)
-                .setImageNormalIds(
+                .setImageNormalIds(                     //设置tab未选择时图片id
                         R.drawable.mainicon_normal,
                         R.drawable.mainicon_normal,
                         R.drawable.mainicon_normal,
                         R.drawable.mainicon_normal)
-                .setTabTextSizeNormalAll(DisplayUtil.scaleValue(this, 24))
-                .setTabTextSizeSelectedAll(DisplayUtil.scaleValue(this, 24))
-                .setTabImgSizeNormalAll(DisplayUtil.scaleValue(this, 50))
-                .setTabImgSizeSelectedAll(DisplayUtil.scaleValue(this, 50))
-                .setTabTextColorResIdNormalAll(R.color.colorTabGray)
-                .setTabTextColorResIdSelectedAll(R.color.colorTabMain)
-                .setMarkNumberSizeAll(DisplayUtil.scaleValue(this, 20))
-                .setMarkWidthAll(DisplayUtil.scaleValue(this, 50))
-                .setMarkHeightAll(DisplayUtil.scaleValue(this, 40))
-                .setMarkNumberAt(0, 100)
-                .showMarkAt(0)
-                .setOnTabClickListener(new ZzFragmentTabHost.OnTabClickListener() {
+                .setTabTextSizeNormalAll(DisplayUtil.scaleValue(this, 24))      //设置tab未选择时文字大小
+                .setTabTextSizeSelectedAll(DisplayUtil.scaleValue(this, 24))    //设置tab选择时文字大小
+                .setTabImgSizeNormalAll(DisplayUtil.scaleValue(this, 50))       //设置tab未按下时图片大小
+                .setTabImgSizeSelectedAll(DisplayUtil.scaleValue(this, 50))     //设置tab按下时图片大小
+                .setTabTextColorResIdNormalAll(R.color.colorTabGray)            //设置tab未按下时文字颜色
+                .setTabTextColorResIdSelectedAll(R.color.colorTabMain)          //设置tab按下时文字颜色
+                .setMarkNumberSizeAll(DisplayUtil.scaleValue(this, 20))         //设置tab角标文字大小
+                .setMarkWidthAll(DisplayUtil.scaleValue(this, 50))              //设置tab角标宽度
+                .setMarkHeightAll(DisplayUtil.scaleValue(this, 40))             //设置tab角标高度
+                .setMarkNumberAt(0, 100)                                        //设置第postion位置的tab对应的角标的值
+                .showMarkAt(0)                                                  //显示第position位置的tab对应的角标
+                .setOnTabClickListener(new ZzFragmentTabHost.OnTabClickListener() {    //tab点击监听
                     @Override
                     public void onTabClick(ZzTab tab, boolean changed, int position) {
 
                     }
                 })
-                .setSelection(0);
+                .setSelection(0);          //默认选中第一个tab
 
     }
 }
---
+++ app/src/main/java/me/zhouzhuo/zzfragmenttabhostdemo/utils/DisplayConfig.java
@@ -0,0 +1,10 @@
+package me.zhouzhuo.zzfragmenttabhostdemo.utils;
+
+/**
+ * Created by zz on 2016/9/11.
+ */
+public class DisplayConfig {
+    public static int SCREEN_WIDTH = 720;
+    public static int SCREEN_HEIGHT = 1080;
+    public static int SCREEN_UI_DENSITY = 4;
+}
